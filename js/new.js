b316d4cc48efae460803b318fb3eed7da0d25e74
unknown <pavithrasivanoli@gmail.com>
2022-12-17 09:28:04 +0530

    updated the files

---
+++ js/new.js
@@ -0,0 +1,22 @@
+/*var x=prompt("enter a number");
+sum=0;
+while(x>0)
+    {
+	var rem=x%10
+	sum=rem+(sum*10)
+	x=(x-rem)/10
+	if(x==sum)
+	{
+		document.write("palindrome")
+	}
+	{
+		document.write("not palindrome")
+	}
+	}*/
+	/*var a=[1,2,3,4,5]
+	sum=0;
+	for (i=0;i<a.length;i++)
+	{
+		sum+=a[i];
+	}
+	console.log(sum);*/
